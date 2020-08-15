12bf332b0996d64b38cfcddb7945b6a0c9023d67
Jon McDuffie <bobber321988@gmail.com>
2020-08-15 10:43:55 -0400

    day 1

---
+++ day-1/src/main.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("day_1_input.txt")?;
+    let mut total_fuel: i32 = 0;
+    for line in io::BufReader::new(file).lines() {
+        if let Ok(mass_string) = line {
+            if let Ok(mass) = mass_string.parse::<i32>() {
+                total_fuel += calculate_fuel(mass);
+            }
+        }
+    }
+    println!("{}", total_fuel);
+    Ok(())
+}
+
+fn calculate_fuel(mass: i32) -> i32 {
+    match mass {
+        0 => mass,
+        _ => {
+            let fuel = mass / 3 - 2;
+            if fuel >= 0 {
+                fuel + calculate_fuel(fuel)
+            } else {
+                0
+            }
+        }
+    }
+}
